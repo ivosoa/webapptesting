56b76694a0d04cb32b3260828698552928f820fa
ivo <Ivo@Ivo-PC.localdomain>
2014-08-12 00:55:15 +0200

    - expanded html code in HtmlGenerator to use angularjs

--- webappShowCase/src/main/groovy/org/webappshowcase/servlet/HtmlGenerator.groovy
+++ webappShowCase/src/main/groovy/org/webappshowcase/servlet/HtmlGenerator.groovy
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.webappshowcase.servlet
 
 /**
@@ -14,13 +8,39 @@ class HtmlGenerator {
   
   def getHtmlPage(){
     def writer = new StringWriter()  
+    writer.write('<!DOCTYPE html>\n')
+    
     def builder = new groovy.xml.MarkupBuilder(writer)
+    
+    builder.doubleQuotes = true
+    builder.expandEmptyElements = true
+    builder.omitEmptyAttributes = false
+    builder.omitNullAttributes = false
     builder.html{
       head{
-        title 'Page generated with Groovy'
+        meta(charset:'utf-8')
+        title 'Page Title'
+        script (src: 'js/angular.min.js', type: 'text/javascript')
+        script (src: 'js/module/angularModule.js', type: 'text/javascript')
+        script (src: 'js/module/controller/listController.js', type: 'text/javascript')
+        script (src: 'js/module/controller/contentController.js', type: 'text/javascript')
+        script (src: 'js/module/factory/ajaxService.js', type: 'text/javascript')
       }
-      body{
+      body('ng-app':'mainModule'){
         h1 'Page generated with Groovy'
+        div(id:'list', 'ng-controller':'listController'){
+          ul('class':'listElems'){
+            li('ng-repeat':'elem in userList', '{{elem.firstName}}')
+          }
+        }
+        div(id:'content', 'ng-controller':'contentController'){
+          form('ng-submit':'addUser()'){
+            input(type:'text', name:'firstName', 'ng-model':'userForm.firstName')
+            input(type:'text', name:'lastName', 'ng-model':'userForm.lastName')
+            input(type:'text', name:'email', 'ng-model':'userForm.email')
+            input(type:'submit', value:'add')
+          }
+        }
       }
     }
     return writer.toString()
