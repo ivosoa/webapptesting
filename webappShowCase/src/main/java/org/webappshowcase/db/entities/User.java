26c51406790525c96b7fa60173cf498261f2749b
ivo <Ivo@Ivo-PC.localdomain>
2014-08-12 00:59:15 +0200

    - removed license comments from all classes - added fields, getters and setters to User Class

--- webappShowCase/src/main/java/org/webappshowcase/db/entities/User.java
+++ webappShowCase/src/main/java/org/webappshowcase/db/entities/User.java
@@ -1,15 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.webappshowcase.db.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
- * @author ivo
+ * @author Ivo
  */
+@Entity
 public class User {
-  
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
 }
